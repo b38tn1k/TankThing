eea43ece0070aaaab0796550a6caa044c4824fce
James Carthew <jamesrobertcarthew@gmail.com>
2016-06-20 21:14:20 -0700

    messing with terrain is fun!

--- classes/world.lua
+++ classes/world.lua
@@ -2,6 +2,8 @@
 local World = {}
 World.__index = World
 
+-- TODO: clean this - make a 'render world' method and seperate it from the creation section
+
 function World.create()
   local w = {}
   setmetatable(w, World)
@@ -15,25 +17,30 @@ function World.create()
       break
     end
   end
-  print (w.seed)
   w.window = {}
   w.window.width, w.window.height, w.window.FLAGS = love.window.getMode()
   -- make terrain canvas thing
-  w.grid = {}
+  w.map = {}
   for i = 1, w.window.width do
-      w.grid[i] = {}
+      w.map[i] = {}
       for j = 1, w.window.height do
-          w.grid[i][j] = love.math.noise(w.seed * i, w.seed * j)
+          w.map[i][j] = love.math.noise(w.seed * i, w.seed * j)
       end
   end
   w.canvas = love.graphics.newCanvas(w.window.width, w.window.height)
   love.graphics.setCanvas(w.canvas)
   for i = 1, w.window.width do
     for j = 1, w.window.height do
-      if w.grid[i][j] < 0.3 then
-        love.graphics.setColor(0, 0, 255, 255)
+      if w.map[i][j] < 0.3 then
+        love.graphics.setColor(50, 100, 255, 255)
+      elseif w.map[i][j] < 0.35 then
+        love.graphics.setColor(200, 150, 0, 255)
+      elseif w.map[i][j] < 0.5 then
+        love.graphics.setColor(80, 100, 0, 255)
+      elseif w.map[i][j] < 0.9 then
+        love.graphics.setColor(40, 100, 40, 255)
       else
-        love.graphics.setColor(0, 255, 0, 255)
+        love.graphics.setColor(30, 80, 10, 255)
       end
       love.graphics.point( i - 1, j - 1 )
     end
