122b0fe0946e4928c7d1573ce55b2c67bfd9eed8
James Carthew <jamesrobertcarthew@gmail.com>
2016-06-20 21:03:03 -0700

    first attempt simplex terrain

--- classes/world.lua
+++ classes/world.lua
@@ -1,12 +1,50 @@
 -- World Class
-local w = {}
+local World = {}
+World.__index = World
 
-w.time = 0
-w.window = {}
-
-local function init()
+function World.create()
+  local w = {}
+  setmetatable(w, World)
+  math.randomseed(os.time() * 1000)
+  temp_seed = 0
+  w.seed = math.random()
+  while true do
+    temp_seed = math.random()
+    if temp_seed < 0.03  and temp_seed > 0.01 then
+      w.seed = temp_seed/10
+      break
+    end
+  end
+  print (w.seed)
+  w.window = {}
   w.window.width, w.window.height, w.window.FLAGS = love.window.getMode()
+  -- make terrain canvas thing
+  w.grid = {}
+  for i = 1, w.window.width do
+      w.grid[i] = {}
+      for j = 1, w.window.height do
+          w.grid[i][j] = love.math.noise(w.seed * i, w.seed * j)
+      end
+  end
+  w.canvas = love.graphics.newCanvas(w.window.width, w.window.height)
+  love.graphics.setCanvas(w.canvas)
+  for i = 1, w.window.width do
+    for j = 1, w.window.height do
+      if w.grid[i][j] < 0.3 then
+        love.graphics.setColor(0, 0, 255, 255)
+      else
+        love.graphics.setColor(0, 255, 0, 255)
+      end
+      love.graphics.point( i - 1, j - 1 )
+    end
+  end
+  love.graphics.setCanvas()
+  return w
 end
-w.init = init
 
-return w
+function World:draw()
+  love.graphics.draw(self.canvas)
+
+end
+
+return World
--- main.lua
+++ main.lua
@@ -1,19 +1,16 @@
 function love.load()
   math.randomseed(os.time())
   love.window.setTitle('UnUntitled')
-  world = require("classes.world")
+  World = require("classes.world")
   tank = require("classes.tank")
   projectile = require("classes.projectile")
-  world.init()
+  world = World.create()
   tank.init(world.window.width/2, world.window.height/2, world.window.width, world.window.height, 400, 600, projectile, "blue_tank_base.png", "blue_tank_turrent.png", "blue_missile.png")
-  wasd = true
-  drift = false
-  click = false
-  follow = false
+  time = 0
 end
 
 function love.update(dt)
-  world.time = world.time + dt
+  time = time + dt
   tank.userControl()
   tank.approachTarget(dt)
   tank.rotate(dt)
@@ -21,6 +18,7 @@ function love.update(dt)
 end
 
 function love.draw()
+  world:draw()
   tank.draw()
   tank.debug_view()
 end
